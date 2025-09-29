fa356cbaae99976ff137c98306bee26f3b7c7fc7
vladislav <vladis27449@yandex.ru>
2025-09-29 20:03:55 +0700

    Add buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import {Text, View, StyleSheet, TouchableOpacity} from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
+const BUTTONS = [
+    ['C', 'DEL', '/', '*'],
+    ['7', '8', '9', '-'],
+    ['4', '5', '6', '+'],
+    ['1', '2', '3', '='],
+    ['0', '.', 'back', '='],
+]
+
+const sizeButton = 70;
+
+export default function StartScreen() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                {
+                    BUTTONS.map((row, rowIndex) => (
+                        <View key={rowIndex} style={styles.row}>
+                            {row.map((char, charIndex) => (
+                                <TouchableOpacity key={charIndex} style={styles.button}>
+                                    <Text>{char}</Text>
+                                </TouchableOpacity>
+                            ))}
+                        </View>
+                    ))
+                }
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Hello world!</Text>
-    </View>
-  );
-}
+    },
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+    },
+    buttons: {
+        backgroundColor: "green",
+        alignSelf: "stretch",
+    },
+    button: {
+        backgroundColor: "yellow",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 10,
+        height: sizeButton,
+        width: sizeButton,
+    },
+    equalButton: {
+        height: sizeButton * 2 + 20, // растянем на две строки (20 = 2 * margin)
+        alignSelf: "stretch",
+    }
+})
